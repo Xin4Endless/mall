08c3b13e35c99cf319e7ca1844296435397fb812
Xin Wb <endless_2010@126.com>
2022-01-25 19:40:02 +0800

    初始化

--- mall-common/src/main/java/com/xin/mallcommon/MallCommonApplication.java
+++ mall-common/src/main/java/com/xin/mallcommon/MallCommonApplication.java
@@ -1,9 +1,13 @@
 package com.xin.mallcommon;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DruidDataSourceAutoConfigure.class})
+@EnableDiscoveryClient
 public class MallCommonApplication {
 
     public static void main(String[] args) {
